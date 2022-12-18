802f4bf451583ad0047183fd096364788925219c
AlexTS3 <tsvetanovalex@yahoo.com>
2022-12-18 11:55:33 +0200

    RegExp added

--- arraysLab/01-sumOfElements.js
+++
@@ -1,9 +0,0 @@
-function sumOfElemenets(numbersArray) {
-
-    let firstNum = numbersArray[0];
-    let lastNum = numbersArray[numbersArray.length - 1];
-
-    console.log(firstNum + lastNum)
-}
-
-sumOfElemenets([10, 17, 22, 33])
